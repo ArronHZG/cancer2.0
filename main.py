655757f28c9a542d9ddf6ef46335bd4789bc148f
houzhenggangGit <hou.zg@foxmail.com>
2019-03-16 01:43:36 +0800

    调整学习率

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ dataloaders = {'train': train_loader, 'valid': valid_loader}
 model = resnet18(num_classes=2, pretrained=False)
 model_name = 'resnet18'
 # 模型参数加载
-model = load_parameter(model,model_name)
+model = load_parameter(model,model_name,imageNet=True)
 # model可视化
 x = torch.rand(1, 3, 32, 32)  # 随便定义一个输入
 writer.add_graph(model, x)
@@ -54,27 +54,22 @@ model.cuda(device)
 # 损失函数
 criterion = torch.nn.CrossEntropyLoss().cuda(device)
 
-# 优化器
-# 使用warm_up
-optimizer = torch.optim.ASGD(model.parameters(), lr=2e-6, lambd=1e-4, alpha=0.75, t0=1e6, weight_decay=1e-6)
-scheduler = StepLR(optimizer, 1, gamma=2)
-# 训练和评估
+# 训练
+optimizer = torch.optim.ASGD(model.parameters(), lr=2e-1, lambd=1e-4, alpha=0.75, t0=1e6, weight_decay=1e-6)
 train_model(model, model_name, dataloaders,
-            criterion, optimizer, device, scheduler, test_size=test_size, num_epochs=[0, 10])
+            criterion, optimizer, device, test_size=test_size, num_epochs=[0, 20])
 
-optimizer = torch.optim.ASGD(model.parameters(), lr=2e-2, lambd=1e-4, alpha=0.75, t0=1e6, weight_decay=1e-6)
+optimizer = torch.optim.ASGD(model.parameters(), lr=2e-1, lambd=1e-4, alpha=0.75, t0=1e6, weight_decay=1e-6)
 scheduler = CosineAnnealingLR(optimizer, T_max=8, eta_min=2e-6)
-# 训练和评估
 train_model(model, model_name, dataloaders,
-            criterion, optimizer, device, scheduler, test_size=test_size, num_epochs=[10, 42])
+            criterion, optimizer, device, scheduler, test_size=test_size, num_epochs=[20, 36])
 
 optimizer = torch.optim.Adam(model.parameters(),lr=2e-2, betas=(0.9, 0.999), eps=1e-8,weight_decay=1e-6, amsgrad=True)
 scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5,
                               verbose=True, threshold=1e-4, threshold_mode='rel',
                               cooldown=0, min_lr=0, eps=1e-86)
-# 训练和评估
 train_model(model, model_name, dataloaders,
-            criterion, optimizer, device, scheduler, test_size=test_size, num_epochs=[42, 70])
+            criterion, optimizer, device, scheduler, test_size=test_size, num_epochs=[36, 50])
 
 # models = PNASNet5Large(2)
 
