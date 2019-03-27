8df7ad4d819460956d518745f93efa8f689b2afa
houzhenggangGit <hou.zg@foxmail.com>
2019-03-28 01:36:37 +0800

    cuda-debug

--- submit.py
+++ submit.py
@@ -7,6 +7,7 @@ from load_paramter import load_parameter
 import pandas as pd
 import torch
 
+from models.pnasnet import pnasnet5large
 from models.resnet import resnet18
 from trainer import START_TIME
 from tta_data_set import TTA_data_set
@@ -16,22 +17,25 @@ NUM_WORKERS = 8
 
 
 def test_epoch(model, data_loaders, device):
-    list = []
+    # 加载到GPU
+    model.cuda(device)
     model.eval()
-    for inputs, labels in tqdm(data_loaders):
-        inputs = inputs.cuda(device)
+    valid_len = vd.count()["id"]
+    list = []
+    for idx in tqdm(range(valid_len)):
+        pics, _ = valid_set[idx]
+        pics = torch.stack(pics)
+        inputs = pics.cuda(device)
         pred = model(inputs)
-        pred = torch.argmax(pred, 1)
+        pred = torch.argmax(pred.mean(dim=0))
         list.extend(pred.cpu().numpy().tolist())
     return list
 
 
 def submit(model, model_name, device, test_path, csv_path):
     sample_df = pd.read_csv(csv_path)
-    valid_set = PCam_data_set(sample_df, test_path, 'test')
-    valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=BATCH_SIZE,
-                                               shuffle=False, num_workers=NUM_WORKERS)
-    np_list = test_epoch(model, valid_loader, device)
+    valid_set = TTA_data_set(sample_df, test_path, tta_times=9)
+    np_list = test_epoch(model, valid_set, device)
     sample_df["label"] = np_list
     if not os.path.exists("submit"):
         os.makedirs("submit")
@@ -45,22 +49,32 @@ if __name__ == '__main__':
     test_csv_url = INPUT_PATH + '/sample_submission.csv'
     device = 0
 
-    # # 加载模型
-    # model = pnasnet5large(num_classes=2, pretrained=False)
-    # model_name = 'pnasnet5large'
-    # # 模型参数加载
-    # model = load_parameter(model,
-    #                        model_name,
-    #                        type='pre_model',
-    #                        pre_model='models_weight/MyWeight/' +
-    #                                   '2019-03-24--15:32:27/' +
-    #                                   '2019-03-27--03:40:56--pnasnet5large--184--Loss--0.0680--Acc--0.9847.pth')
-    #
-    # # 加载到GPU
-    # model.cuda(device)
-    # submit(model, model_name, device, test_path, test_csv_url)
+    # 加载模型
+    model = resnet18(num_classes=2, pretrained=False)
+    model_name = 'resnet18'
+    # 模型参数加载
+    model = load_parameter(model,
+                           model_name,
+                           type='pre_model',
+                           pre_model='models_weight/MyWeight/' +
+                                     '2019-03-17--14:34:45/' +
+                                     '2019-03-17--17:38:43--resnet18--46--Loss--0.0699--Acc--0.9767.pth')
+    submit(model, model_name, device, test_path, test_csv_url)
 
-    test = True
+    # 加载模型
+    model = pnasnet5large(num_classes=2, pretrained=False)
+    model_name = 'pnasnet5large'
+    # 模型参数加载
+    model = load_parameter(model,
+                           model_name,
+                           type='pre_model',
+                           pre_model='models_weight/MyWeight/' +
+                                      '2019-03-24--15:32:27/' +
+                                      '2019-03-27--03:40:56--pnasnet5large--184--Loss--0.0680--Acc--0.9847.pth')
+
+    submit(model, model_name, device, test_path, test_csv_url)
+
+    test = False
     if test:
         train_csv_url = INPUT_PATH + '/train_labels.csv'
         data = pd.read_csv(train_csv_url)
@@ -92,14 +106,10 @@ if __name__ == '__main__':
             pics, label = valid_set[idx]
             pics = torch.stack(pics)
             labels = torch.FloatTensor([label] * pics.size()[0]).long()
-
             inputs = pics.cuda(device)
             labels = labels.cuda(device)
-
             pred = model(inputs)
-
             valid_loss += criterion(pred, labels).item()
-
             pred = torch.argmax(pred, 1)
             correct = pred.size(0) - (pred ^ label).sum().item()
             sample = pred.size(0)
@@ -107,3 +117,4 @@ if __name__ == '__main__':
         valid_loss /= valid_len
         valid_acc /= valid_len
         print(f"valid_loss: {valid_loss},valid_acc: {valid_acc}")
+        # valid_loss: 0.08269919017589364, valid_acc: 0.971802563403329
