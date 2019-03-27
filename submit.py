d638198af450ac9a1246717667a93ffa98fd6047
houzhenggangGit <hou.zg@foxmail.com>
2019-03-28 01:06:09 +0800

    cuda-debug

--- submit.py
+++ submit.py
@@ -91,9 +91,11 @@ if __name__ == '__main__':
         for idx in tqdm(range(valid_len)):
             pics, label = valid_set[idx]
             pics = torch.stack(pics)
-            labels = torch.Tensor([label] * pics.size()[0], dtype=torch.float)
+            labels = torch.FloatTensor([label] * pics.size()[0])
+
             inputs = pics.cuda(device)
             labels = labels.cuda(device)
+
             pred = model(inputs)
 
             valid_loss += criterion(pred, labels).item()
