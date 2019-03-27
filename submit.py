852107fac90b8078819ef3b785d009790f2119c0
houzhenggangGit <hou.zg@foxmail.com>
2019-03-28 00:52:55 +0800

    cuda-debug

--- submit.py
+++ submit.py
@@ -62,6 +62,13 @@ if __name__ == '__main__':
 
     test = True
     if test:
+        train_csv_url = INPUT_PATH + '/train_labels.csv'
+        data = pd.read_csv(train_csv_url)
+        train_path = INPUT_PATH + '/train/'
+        _, vd = train_test_split(data, test_size=0.1, random_state=123)
+        valid_set = TTA_data_set(vd, train_path, tta_times=9)
+        valid_len = vd.count()["id"]
+
         # 加载模型
         model = resnet18(num_classes=2, pretrained=False)
         model_name = 'resnet18'
@@ -73,22 +80,18 @@ if __name__ == '__main__':
                                          '2019-03-17--14:34:45/' +
                                          '2019-03-17--17:38:43--resnet18--46--Loss--0.0699--Acc--0.9767.pth')
 
-        train_csv_url = INPUT_PATH + '/train_labels.csv'
-        data = pd.read_csv(train_csv_url)
-        train_path = INPUT_PATH + '/train/'
-        _, vd = train_test_split(data, test_size=0.1, random_state=123)
-        valid_set = TTA_data_set(vd, train_path, tta_times=9)
-        valid_len = vd.count()["id"]
+        model.cuda(device)
+        model.eval()
 
         # 损失函数
         criterion = torch.nn.CrossEntropyLoss().cuda(device)
-        model.eval()
+        # 评估
         valid_loss = 0
         valid_acc = 0
         for idx in tqdm(range(valid_len)):
             pics, label = valid_set[idx]
             pics = torch.stack(pics)
-            labels = torch.Tensor([label]*pics.size()[0])
+            labels = torch.Tensor([label] * pics.size()[0])
             inputs = pics.cuda(device)
             labels = labels.cuda(device)
             pred = model(inputs)
@@ -100,6 +103,5 @@ if __name__ == '__main__':
             sample = pred.size(0)
             valid_acc += correct / sample
         valid_loss /= valid_len
-        valid_acc/=valid_len
+        valid_acc /= valid_len
         print(f"valid_loss: {valid_loss},valid_acc: {valid_acc}")
-
